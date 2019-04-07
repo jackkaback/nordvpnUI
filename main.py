75944e7b9b18b3fdf169b0695e3d98612bbe05e1
jack <jackkaback@gmail.com>
2019-04-07 01:18:19 -0400

    added city support

--- main.py
+++ main.py
@@ -1,6 +1,92 @@
 #!/usr/bin/env python3
 import os
+import subprocess
 from tkinter import *
+
+
+# parses the data from the console
+def parseInput(data):
+
+	# Turns the data from a lot of giberish into the output
+	data = str(data.stdout)
+	data = data.replace("\\r", "--")
+	data = data.replace("\\n", "--")
+	data = data.replace("\\t", "--")
+	data = data.replace("\\", "--")
+	data = data.split("-")
+
+	ret_val = []
+
+	# removes assorted trash data
+	for i in range(len(data)):
+		if len(data[i]) >= 3:
+			ret_val.append(data[i])
+
+	ret_val = sorted(ret_val)
+
+	return ret_val
+
+
+# gets the countries in the list
+def getCountries():
+
+	country_list = subprocess.run(['nordvpn', 'countries'], stdout=subprocess.PIPE)
+
+	country_list = parseInput(country_list)
+
+	# added any to support connecting to random server
+	country_list.append("Any")
+	return country_list
+
+
+# gets cities from a country
+def getCities(country):
+	city_list = subprocess.run(['nordvpn', 'cities', country], stdout=subprocess.PIPE)
+
+	return parseInput(city_list)
+
+# GUI for geting country
+def guiCountry():
+
+	base = Tk()
+	base.title("Nord VPN setup")
+	base.geometry("300x150")
+
+	# list of currently supported countries
+	region_list = getCountries()
+
+	region = StringVar(base)
+	region.set(region_list[0])  # default value
+
+	# sets up the drop down menu
+	w = OptionMenu(base, region, *region_list)
+	w.pack()
+
+	# if the user wants to city select
+	pickCity = IntVar()
+	Checkbutton(base, text="city select", variable=pickCity).pack()
+
+	mainloop()
+
+	return [region.get(), pickCity.get()]
+
+# GUI for selecting city
+def guiCity(cities):
+
+	base = Tk()
+	base.title("City selector")
+	base.geometry("300x150")
+
+	region = StringVar(base)
+	region.set(cities[0])  # default value
+
+	# sets up the drop down menu
+	w = OptionMenu(base, region, *cities)
+	w.pack()
+
+	mainloop()
+
+	return region.get()
 
 
 # connects to the location
@@ -14,31 +100,21 @@ def disconnect():
 
 
 def main():
-	base = Tk()
-	base.title("Nord VPN setup")
-	base.geometry("300x150")
+	tempArr = guiCountry()
 
+	# picking any makes the VPN decide the location
+	if tempArr[0] == "Any":
+		connect("")
 
-	# list of currently supported countries
-	region_list = ["Albania", "Argentina", "Australia", "Austria", "Belgium", "Bosnia_And_Herzegovina", "Brazil",
-				"Bulgaria", "Canada", "Chile", "Costa_Rica", "Croatia", "Cyprus", "Czech_Republic", "Denmark",
-				"Estonia", "Finland", "France", "Georgia", "Germany", "Greece", "Hong_Kong", "Hungary", "Iceland",
-				"India", "Indonesia", "Ireland", "Israel", "Italy", "Japan", "Latvia", "Luxembourg", "Malaysia",
-				"Mexico", "Moldova", "Netherlands", "New_Zealand", "North_Macedonia", "Norway", "Poland", "Portugal",
-				"Romania", "Serbia", "Singapore", "Slovakia", "Slovenia", "South_Africa", "South_Korea", "Spain",
-				"Sweden", "Switzerland", "Taiwan", "Thailand", "Turkey", "Ukraine", "United_Kingdom",
-				"United_States", "Vietnam"]
+	# pick the city GUI for specific city
+	elif tempArr[1] == 1:
+		cities = getCities(tempArr[0])
+		city = guiCity(cities)
+		connect(tempArr[0] + " " + city)
 
-	region = StringVar(base)
-	region.set(region_list[0])  # default value
-
-	# sets up the drop down menu
-	w = OptionMenu(base, region, *region_list)
-	w.pack()
-
-	mainloop()
-
-	connect(region.get())
+	# connect to whatever server in the target country
+	else:
+		connect(tempArr[0])
 
 
 if __name__ == '__main__':
